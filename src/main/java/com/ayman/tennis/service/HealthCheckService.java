cf71f670562a0598afba3ead61a8cd6ca75882dd
Ayman-Bsb <ayman1bouassab@gmail.com>
2024-05-26 18:02:17 +0200

    [set_up] - add HealthCheckService

---
+++ src/main/java/com/ayman/tennis/service/HealthCheckService.java
@@ -0,0 +1,22 @@
+package com.ayman.tennis.service;
+
+import com.ayman.tennis.ApplicationStatus;
+import com.ayman.tennis.HealthCheck;
+import com.ayman.tennis.repository.HealthCheckRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HealthCheckService {
+    @Autowired
+    private HealthCheckRepository healthCheckRepository;
+    public HealthCheck healthCheck() {
+        Long activeSessions = healthCheckRepository.countApplicationConnections();
+        if(activeSessions > 0){
+            return new HealthCheck(ApplicationStatus.OK, "Welcome to my App!");
+        }
+        else {
+            return new HealthCheck(ApplicationStatus.KO, "The App is not fully functional, please check your configuration.!");
+        }
+    }
+}
