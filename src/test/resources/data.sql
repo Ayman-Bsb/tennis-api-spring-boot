061fdb555ab3faafb128ae029c480400b4bced71
Ayman-Bsb <ayman1bouassab@gmail.com>
2024-06-09 01:34:21 +0200

    [tests] [integration] - using the H2 database for faster and lightweight in-memory usage

---
+++ src/test/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO player(last_name, first_name, birth_date, position, points)
+	VALUES ('NadalTest', 'Rafael', '1986-06-03', 1, 5000);
+
+INSERT INTO player(last_name, first_name, birth_date, position, points)
+    	VALUES ('DjokovicTest', 'Novak', '1987-05-22', 2, 4000);
+
+INSERT INTO player(last_name, first_name, birth_date, position, points)
+    	VALUES ('FedererTest', 'Roger', '1981-08-08', 3, 3000);
